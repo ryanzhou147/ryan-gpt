30df8a79f590412a55e94a0473760ddcdf8bdec2
Ryan Zhou <ryanawesome888@gmail.com>
2025-11-08 13:19:01 -0500

    Made working file splitter by special token

--- cs336_basics/file_content_split.py
+++ cs336_basics/file_content_split.py
@@ -1,27 +1,73 @@
 # Given a file, we want to find all the places where it has <|endofbytes|>
-# Then generate a list of it and chunk it accordingly to NUM_CHUCKS
-from typing import BinaryIO
+# Then generate a list of it and chunk it accordingly to NUM_PROCESSES
+
 import os
+import multiprocessing as mp
+from typing import BinaryIO
 
-def find_EOF_boundaries(file: BinaryIO, num_chunks: int, token: bytes):
-    """
-    Find all positions in the binary file where `token` occurs,
-    and return a list of chunk boundaries that split the file into
-    roughly `num_chunks` parts, aligned to those token positions.
-    """
-    assert isinstance(token, bytes), "token must be in bytes"
+NUM_PROCESSES = 4
 
-    # Get file size
+def find_EOF_boundaries(
+    file: BinaryIO,
+    desired_num_chunks: int,
+    split_special_token: bytes,
+) -> list[int]:
+    """
+    Chunk the file into parts that can be counted independently.
+    May return fewer chunks if the boundaries end up overlapping.
+    Only reads mini_chunk_size at a time to save memory.
+    """
+    assert isinstance(split_special_token, bytes), "Must represent special token as a bytestring"
+
+    # Get total file size in bytes
     file.seek(0, os.SEEK_END)
     file_size = file.tell()
     file.seek(0)
 
-    print(file_size)
+    chunk_size = file_size // desired_num_chunks
 
-    file_content = file.read()
+    # Initial guesses for chunk boundary locations, uniformly spaced
+    # Chunks start on previous index, don't include last index
+    chunk_boundaries = [i * chunk_size for i in range(desired_num_chunks + 1)]
+    chunk_boundaries[-1] = file_size
 
+    mini_chunk_size = 4096  # Read ahead by 4k bytes at a time
+
+    for bi in range(1, len(chunk_boundaries) - 1):
+        initial_position = chunk_boundaries[bi]
+        file.seek(initial_position)  # Start at boundary guess
+        while True:
+            mini_chunk = file.read(mini_chunk_size)  # Read a mini chunk
+
+            # If EOF, this boundary should be at the end of the file
+            if mini_chunk == b"":
+                chunk_boundaries[bi] = file_size
+                break
+
+            # Find the special token in the mini chunk
+            found_at = mini_chunk.find(split_special_token)
+            if found_at != -1:
+                chunk_boundaries[bi] = initial_position + found_at
+                break
+            initial_position += mini_chunk_size
+
+    # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
+    return sorted(set(chunk_boundaries))
+
+
+## Usage 
 current_directory = os.getcwd()
 input_path = os.path.join(current_directory, "cs336_basics/test.txt")
 
 with open(input_path, "rb") as f:
-    find_EOF_boundaries(f, 4, b"<|endoftext|>")
+
+    boundaries = find_EOF_boundaries(f, NUM_PROCESSES, b"<|endoftext|>")
+    print(boundaries)
+
+    # The following is a serial implementation, but you can parallelize this
+    # by sending each start/end pair to a set of processes.
+    for start, end in zip(boundaries[:-1], boundaries[1:]):
+        f.seek(start)
+        chunk = f.read(end - start).decode("utf-8", errors="ignore")
+        # Run pre-tokenization on your chunk and store the counts for each pre-token
+
