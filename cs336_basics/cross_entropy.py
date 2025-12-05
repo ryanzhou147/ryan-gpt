492fed49621fda14d9b1e34294c234213d4d4783
Ryan Zhou <ryanawesome888@gmail.com>
2025-12-04 20:42:05 -0500

    Wrote module for cross entropy loss

--- cs336_basics/cross_entropy.py
+++ cs336_basics/cross_entropy.py
@@ -9,5 +9,22 @@
 # to test your implementation.
 
 import torch
-import torch.init.nn as nn
-class cr
+import torch.nn as nn
+
+class CrossEntropyLoss(nn.Module):
+
+    def __init__(self, logits: torch.Tensor, targets: torch.Tensor) -> None:
+        super().__init__()
+        self.logits = logits
+        self.targets = targets
+    
+    def forward(self) -> torch.Tensor:
+        
+        # Subtract max for numerical stability
+        x_max = torch.max(self.logits, dim=-1, keepdim=True).values
+        self.logits = self.logits - x_max
+        # Compute log probabilities
+        log_probs = self.logits - torch.log(torch.sum(torch.exp(self.logits), dim=-1, keepdim=True))
+        # Gather the log probabilities of the target classes
+        nll_loss = -log_probs.gather(dim=-1, index=self.targets.unsqueeze(-1)).squeeze(-1)
+        return nll_loss.mean()
